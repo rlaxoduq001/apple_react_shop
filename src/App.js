af0ac34594c110d1e5ecfa6ea79624a14a1043b4
rlaxoduq001 <rlaxoduq001@gmail.com>
2022-04-26 23:53:34 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" class="btn btn-warning">Warning</button>
+    </div>
+
+    
+  );
+}
+
+export default App;
