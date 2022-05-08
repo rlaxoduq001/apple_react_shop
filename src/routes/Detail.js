21ef0257a05a85872c8e1afa1c2890dcf1f34884
rlaxoduq001 <rlaxoduq001@gmail.com>
2022-05-08 23:18:27 +0900

    useEffect 2강 , 숙제

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -46,16 +46,56 @@ const Detail = (props) => {
     return x.id == id;
   })
 
+  let [ alert , setAlert] = useState(true);
+  let [ inputVal , setInputVal ] = useState('');
+
   // useEffect Lifecycle
   useEffect( () => {
     // mount,update시 실행
-    console.log("실행");
+    // console.log("실행");
 
-    console.log(divRef.current);
-    setTimeout(() => {
-      divRef.current.style.display = 'none';  
-    }, 2000);
-  })
+    // console.log(divRef.current);
+    // setTimeout(() => {
+    //   divRef.current.style.display = 'none';  
+    // }, 2000);
+
+    let a = setTimeout(() => { setAlert(false) }, 2000);
+    // console.log("2");
+
+    numberCheck( inputVal );
+
+    // useEffect 실행되기 전 작동
+    return() => {
+      // 기존 타이머 제거
+      // console.log("1");
+      clearTimeout(a);
+    }
+  }, [inputVal])
+
+  const onChange = (e) => {
+    setInputVal(e.target.value);
+  }
+
+  const numberCheck = (val) => {
+    if( !val.replace(/[^0-9]/g, '') ){
+      console.log(val);
+      console.log("숫자만 입력");
+    }else {
+      console.log(val);
+      console.log("222");
+    }
+  }
+
+  // userEffect !
+  // useEffect(()=> {}) 1. 재랜더링 마다 코드실행하고 싶으면
+  // useEffect(()=> {}, []) 2. mount시 1회 코드실행하고 싶으면
+  // useEffect(()=> {
+  //   return () => {
+  //     3. unmount시 1회 코드실행하고 싶으면
+  //   }
+  // }) 
+  // 4. userEffect 실행전에 뭔가 실행하려면 return
+  // useEffect(()=> {}, [count]) 5. 특정 state 변경시에만 실행
 
   return (
     <div className="container">
@@ -63,10 +103,21 @@ const Detail = (props) => {
       {/* update시 render*/}
       {count}
       <button onClick={ () => { setCount(count+1) }}>증가</button>
+      {
+        alert == true ? 
+        
+        <div className="alert alert-warning" ref={divRef}>
+          2초이내 구매시 할인
+        </div>
+        :
+        null
+      }
 
-      <div className="alert alert-warning" ref={divRef}>
+      <input type="text" onChange={onChange} value={inputVal}></input>      
+      <div>{inputVal}</div>
+      {/* <div className="alert alert-warning" ref={divRef}>
         2초이내 구매시 할인
-      </div>
+      </div> */}
 
       {/* <Box>
         <YellowBtn bg="blue">버튼</YellowBtn>
